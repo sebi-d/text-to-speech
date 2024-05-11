e5a7561cbf49890fcc75918de7ef7b23c4466797
sebi <91790390+andreisdodot@users.noreply.github.com>
2024-05-11 17:56:21 +0300

    added conversion

--- index.php
+++ index.php
@@ -1,9 +1,42 @@
 <?php
 
-echo "Hello World!";
+$endpoint = 'https://westeurope.api.cognitive.microsoft.com/';
+$key = '87272c46530b46ca945291d4bcd65bc9';
+$region = 'westeurope';
 
-// add a form to the page
-echo "<form action='convert.php' method='post'>
-    <input type='text' name='name'>
-    <input type='submit' value='Submit'>
-</form>";
+try {
+    $text = $_POST['text'];
+
+    $url = "https://westeurope.tts.speech.microsoft.com/cognitiveservices/v1";
+
+    $headers = array(
+        "Ocp-Apim-Subscription-Key: $key",
+        "Content-Type: application/ssml+xml",
+        "X-Microsoft-OutputFormat: audio-24khz-160kbitrate-mono-mp3",
+        "User-Agent: curl",
+    );
+
+    $requestBody = "<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='en-US-AriaNeural'>$text</voice></speak>";
+
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    if(curl_errno($ch)){
+        echo 'Curl error: ' . curl_error($ch);
+    }
+
+    curl_close($ch);
+
+    header("Content-Type: audio/mpeg");
+    echo $response;
+    
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
+?>
