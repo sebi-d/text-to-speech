d6294003447f054800c647cabb4aa0b32869abda
sebi <91790390+andreisdodot@users.noreply.github.com>
2024-05-11 16:45:18 +0300

    added form

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 echo "Hello World!";
+
+// add a form to the page
+echo "<form action='convert.php' method='post'>
+    <input type='text' name='name'>
+    <input type='submit' value='Submit'>
+</form>";
